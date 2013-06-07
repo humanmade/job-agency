fa2a7a6fd12c8f771e66ea215709aec215f318de
Joe Hoyle <joehoyle@gmail.com>
2013-06-07 17:55:55 +0100

    Only require wp-cli command if it's loaded, create table on admin init

--- job-agency.php
+++ job-agency.php
@@ -8,16 +8,8 @@
 require_once 'class-job-agency.php';
 require_once 'class-job-agency-job.php';
 require_once 'class-job-agency-worker.php';
-require_once 'class-job-agency-cli-command.php';
 
+if ( defined( 'WP_CLI' ) && WP_CLI )
+	require_once 'class-job-agency-cli-command.php';
 
-add_action( 'init', function() {
-
-	//Job_Agency::add_job( 'upload-backup', array( 'site_id' => 1 ) );
-
-	$job = Job_Agency::get_new_job();
-	$job->complete();
-
-	var_dump($job);
-	exit;
-});
+add_action( 'admin_init', array( 'Job_Agency', 'create_table' ) );
