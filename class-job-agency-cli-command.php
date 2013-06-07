687e531a1442ecfb8eb7a1273bf43b56acb1fc52
Joe Hoyle <joehoyle@gmail.com>
2013-06-07 17:55:14 +0100

    Added WP_CLI command for Job_Agency

--- class-job-agency-cli-command.php
+++ class-job-agency-cli-command.php
@@ -1,2 +1,28 @@
 <?php
 
+class Job_Agency_CLI_Command extends WP_CLI_Command {
+
+	/**
+	 * Find work for this process, will make this process now block
+	 * 
+	 * @subcommand find-work
+	 */
+	public function find_work() {
+
+		$worker = Job_Agency_Worker();
+
+		$worker->start_work();
+	}
+
+	/**
+	 * Check how many jobs there are to be done
+	 * 
+	 * @subcommand check-employment-needs
+	 */
+	public function check_employment_needs() {
+
+		WP_CLI::line( Job_Agency::get_jobs_available_count() );
+	}
+}
+
+WP_CLI::add_command( 'job-agency', 'Job_Agency_CLI_Command' );
