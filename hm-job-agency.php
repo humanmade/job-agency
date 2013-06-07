5e7645a78307e95ebf5ddd2bcb2518cc93485b41
Joe Hoyle <joehoyle@gmail.com>
2013-06-07 16:46:53 +0100

    Added HM_Job_Agency main file, static methods all around, as this isn't meant to be used for than once. Also, there is no "state", because this is cross-process we don't want things like running jobs stored in memory.

---
+++ hm-job-agency.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Plugin Name: HM Job Agency
+ * 
+ */
+
+class HM_Job_Agency {
+
+	/**
+	 * Create the jobs table
+	 */
+	public static function create_table() {
+
+		global $wpdb;
+
+		$wpdb->query( "CREATE TABLE `" . $this->get_table_name() . "` (
+			`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+			`type` varchar(300) NOT NULL DEFAULT '',
+			`created_date` datetime NOT NULL,
+			`completed_date` datetime DEFAULT NULL,
+			`status` varchar(300) DEFAULT NULL,
+			`payload` longtext,
+			PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT") ;
+	}
+
+	/**
+	 * Add a new job to the job queue
+	 * 
+	 * @param $type string
+	 * @param $payload mixed
+	 */
+	public static function add_job( $type, $payload ) {
+
+		$wpdb->insert(
+			$this->get_table_name(),
+			array( 'type' => $type, 'created_date' => date( 'Y-m-d H:i:s' ), json_encode( $paylaod ) ),
+		);
+	}
+
+	/**
+	 * Get a new job which has not been started
+	 */
+	public static function get_new_job() {
+
+		$wpdb->get_row(
+			"SELECT * FROM " . $this->get_table_name() . " WHERE status = '' LIMIT 1"
+		);
+	}
+
+	/**
+	 * Get the table name for the job queue
+	 */
+	private static function get_table_name() {
+
+		global $wpdb;
+		return $wpdb->prefix . 'hm_jobs';
+	}
+
+
+}
