b4771c38842582ca3c422382f3374f656cda98fd
Joe Hoyle <joehoyle@gmail.com>
2013-06-07 18:45:23 +0100

    Each time a worker finishes a job, check to see if it was fired

--- class-job-agency-worker.php
+++ class-job-agency-worker.php
@@ -2,13 +2,27 @@
 
 class Job_Agency_Worker {
 
+	public $date_started_work = null;
+
 	public function start_work() {
 
+		$this->date_started_work = time();
+
 		while( $job = Job_Agency::get_new_job() ) {
-			
+
 			$job->start();
 			$job->call_handler();
 			$job->completed();
+
+			if ( $this->is_fired() )
+				return;
 		}
+	}
+
+	private function is_fired() {
+		if ( $fire_date = Job_Agency::get_last_fire_date() )
+			return $fire_date > $this->date_started_work;
+
+		return false;
 	}
 }
